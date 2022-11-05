59783ed095e9c911345be514af536dd88fe1cf03
FinaevIgor <finaevigor@yandex.ru>
2022-11-05 19:10:48 +0300

    Задание 2

---
+++ r.py
@@ -0,0 +1,6 @@
+num = int(input("Введите высоту прямоугольника :"))
+num2 = int(input("Введите ширину прямоугольника :"))
+for i in range(num):
+    print()
+    for i in range(num2):
+        print("*",end="")
